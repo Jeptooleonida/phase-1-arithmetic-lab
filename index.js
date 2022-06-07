3ad8c53dd4c48d5d2a8fd0aab12a2dac3b1a1008
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-08 00:20:07 +0300

    commit tree 368df03b4e54d0487a5ad4698dd3ee942f7b6623

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+// multiply 
+let num1 = 31; 
+let num2 =2;
+let multiply = (num1*num2);
+
+//random number
+
+let random = Math.floor( Math.random() * 1000 ) + 1;
+//mod
+
+let num3 = 39;
+let num4 = 5;
+let mod = (num3 % num4);
+
+// max
+let max = Math.max(15, 14, 6, 8, 20, 5);
